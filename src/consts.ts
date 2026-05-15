a4c793a3a314162cde54b90c652c1e0a19e9e5ef
Senri_Akane <1545004112@qq.com>
2026-05-15 17:34:52 +0800

    Use readable Chinese source text

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = '\u4e2a\u4eba\u535a\u5ba2';
-export const SITE_DESCRIPTION =
-	'\u8bb0\u5f55\u6280\u672f\u3001\u9605\u8bfb\u548c\u957f\u671f\u601d\u8003\u7684\u4e2a\u4eba\u7a7a\u95f4\u3002';
+export const SITE_TITLE = '个人博客';
+export const SITE_DESCRIPTION = '记录技术、阅读和长期思考的个人空间。';
